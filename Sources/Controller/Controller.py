f05c27b2e16d32193652db91705af94c2fe94728
SerialLooser <haroldducept@gmail.com>
2017-11-11 19:05:27 +0100

    Added first files to the project

---
+++ Sources/Controller/Controller.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+
+class Controller:
+    test = None
+
+    def __init__(self):
+        self.test = "Hello"
+
+    def func(self):
+        print self.test
---
+++ Sources/main.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+from Controller.Controller import Controller
+
+
+if __name__ == '__main__':
+    con = Controller()
+    con.func()
