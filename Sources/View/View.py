61c56690bb28da202526f89f5a09738ab0bfaa1d
AntidotRepository <haroldducept@gmail.com>
2018-12-20 17:22:38 +0100

    Canvas ready

--- Sources/View/View.py
+++ Sources/View/View.py
@@ -1,12 +1,16 @@
 import Tkinter as tk
 
+WIN_WIDTH = 750
+WIN_HEIGHT = 400
+
 
 class View(tk.Toplevel):
     def __init__(self, master):
         tk.Toplevel.__init__(self, master)
         self.protocol('WM_DELETE_WINDOW', self.master.destroy)
         can = tk.Canvas(self,
-                        width=250,
-                        height=140)
-        can.create_line(125, 70, 0, 0, fill="black", width="10")
+                        width=WIN_WIDTH,
+                        height=WIN_HEIGHT)
+        can.create_arc(WIN_WIDTH / 2, WIN_HEIGHT / 2, 0, 0,
+                       width="10", extent=30)
         can.pack()
