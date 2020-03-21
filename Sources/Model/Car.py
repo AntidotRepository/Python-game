085763be8757b42fb55f9c1b7fe37dca84a5f648
AntidotRepository <haroldducept@gmail.com>
2020-03-21 17:45:24 +0100

    Rename variable

--- Sources/Model/Car.py
+++ Sources/Model/Car.py
@@ -2,28 +2,28 @@ from Observable import Observable
 
 
 class Car:
-    def __init__(self, width, height):
+    def __init__(self, w, h):
         self.myPos = Observable(0)
-        self.posX = 0
-        self.posY = 0
-        self.width = width
-        self.height = height
+        self.x = 0
+        self.y = 0
+        self.w = w
+        self.h = h
         self.speed = 5
 
     def set_pos(self, x, y):
-        self.posX = x
-        self.posY = y
-        self.myPos.set((self.posX, self.posY))
+        self.x = x
+        self.y = y
+        self.myPos.set((self.x, self.y))
 
     def move(self, x, y, minX, maxX, minY, maxY):
-        self.posX += self.speed * x
-        if self.posX < minX:
-            self.posX = minX
-        if self.posX > (maxX - self.width):
-            self.posX = maxX - self.width
-        self.posY += self.speed * y
-        if self.posY < minY:
-            self.posY = minY
-        if self.posY > (maxY - self.height):
-            self.posY = (maxY - self.height)
-        self.myPos.set((self.posX, self.posY))
+        self.x += self.speed * x
+        if self.x < minX:
+            self.x = minX
+        if self.x > (maxX - self.w):
+            self.x = maxX - self.w
+        self.y += self.speed * y
+        if self.y < minY:
+            self.y = minY
+        if self.y > (maxY - self.h):
+            self.y = (maxY - self.h)
+        self.myPos.set((self.x, self.y))
